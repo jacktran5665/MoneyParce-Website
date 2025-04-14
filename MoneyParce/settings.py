1ae67f2ea58a92371d1e284a5eaa47dc0b13e9f1
Austin <azhang479@gatech.edu>
2025-04-13 23:11:15 -0400

    fixed redirects and budget error

--- MoneyParce/settings.py
+++ MoneyParce/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-APPEND_SLASH = False
+APPEND_SLASH = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
