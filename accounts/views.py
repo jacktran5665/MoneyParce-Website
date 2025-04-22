1487d6ca986da8e1d45b664fffcac380a2300c7c
Jack Tran <jacktran14122004@gmail.com>
2025-04-22 01:10:22 -0400

    Fixed some layout bugs and cleaned up the profile redirect.

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from .forms import CustomUserCreationForm
-from django.shortcuts import redirect
+
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
+
     if request.method == 'GET':
         template_data['form'] = CustomUserCreationForm()
-        return render(request, 'accounts/register.html',
-            {'template_data': template_data})
+        return render(request, 'accounts/register.html', {'template_data': template_data})
+
     elif request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('dashboard')
         else:
             template_data['form'] = form
-            return render(request, 'accounts/register.html',
-                {'template_data': template_data})
+            return render(request, 'accounts/register.html', {'template_data': template_data})
