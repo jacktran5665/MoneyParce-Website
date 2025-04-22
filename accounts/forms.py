7be289859dc81b8c16966ed3f5f0f0633164a3e2
Jack Tran <jacktran14122004@gmail.com>
2025-04-22 10:28:39 -0400

    Fixed some layout bugs and cleaned up the profile redirect.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
+
+
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1',
-        'password2']:
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your username'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirm your password'
+        })
+
+        for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update(
-                {'class': 'form-control'}
-            )
